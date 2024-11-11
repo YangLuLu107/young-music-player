fc0f7162c6c29a500cd2d4524678745805effd82
yll <973853942@qq.com>
2024-11-11 17:48:05 +0800

    播放列表页面开发

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,33 +4,6 @@
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-/*
-#[derive(Serialize, Deserialize)]
-struct FileEntry {
-    path: String,
-    is_directory: bool,
-}
-#[tauri::command]
-fn get_directory_contents(path: &str) -> Result<Vec<FileEntry>, String> {
-    let path = PathBuf::from(path);
-    if !path.exists() || !path.is_dir() {
-        return Err(format!("路径不存在或不是一个目录: {}", path.display()));
-    }
-
-    let mut entries = Vec::new();
-    for entry in fs::read_dir(path).map_err(|e| e.to_string())? {
-        let entry = entry.map_err(|e| e.to_string())?;
-        let path = entry.path();
-        let metadata = entry.metadata().map_err(|e| e.to_string())?;
-        let is_directory = metadata.is_dir();
-        entries.push(FileEntry {
-            path: path.to_string_lossy().to_string(),
-            is_directory,
-        });
-    }
-
-    Ok(entries)
-}*/
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -38,6 +11,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/components/PlayList.vue
+++ src/components/PlayList.vue
@@ -9,13 +9,16 @@
       <n-tree
           block-line
           draggable
-          :data="data"
-          :expanded-keys="expandedKeys"
+          :data="treeData"
+          :expanded-keys="expandedKeysRef"
           :render-switcher-icon="renderSwitcherIcon"
           :node-props="nodeProps"
           @drop="handleDrop"
-          @update:expanded-keys="handleExpandedKeysChange"
-      />
+          @update:expanded-keys="handleExpandedKeysChange">
+        <template #empty>
+          <span></span>
+        </template>
+      </n-tree>
       <n-dropdown
           trigger="manual"
           placement="bottom-start"
@@ -29,9 +32,10 @@
   </div>
 </template>
 <script setup lang="ts">
-  import { h, ref } from "vue";
+  import {h, onMounted, ref, reactive } from "vue";
   import { readDir, BaseDirectory } from '@tauri-apps/plugin-fs';
   import { open } from '@tauri-apps/plugin-dialog';
+  import { Store, load } from '@tauri-apps/plugin-store';
   import { NDropdown, NButton, NTree, NDivider, NIcon } from 'naive-ui';
   import type { TreeDropInfo, TreeOption } from 'naive-ui';
   import { QueueMusicRound } from '@vicons/material'
@@ -39,16 +43,12 @@
   import { EditOutlined as EditIcon } from '@vicons/antd'
   import { Delete16Regular as DeleteIcon } from '@vicons/fluent'
 
+  const store = ref<Store>();
   const showDropdown = ref(false)
   const x = ref(0)
   const y = ref(0)
   const expandedKeysRef = ref<string[]>([])
-  const dataRef = ref([{
-    label: '默认播放列表',
-    key: '00',
-    children: []
-  }])
-
+  const treeData = ref([])
   const dropdownOptions = [
     {
       label: '新增',
@@ -66,59 +66,136 @@
       icon: renderIcon(DeleteIcon)
     }
   ]
+  onMounted(async () => {
+    store.value = await load('store.json', { autoSave: false });
+    await store.value.clear();
+    const playList = await getStoreData('play-list', store.value);
+    if (!playList) {
+      treeData.value = [{
+        label: '默认播放列表',
+        key: '00',
+        isDirectory: true,
+        children: []
+      }];
+      await setStoreData('play-list', treeData.value, store.value);
+    } else {
+      treeData.value = playList;
+    }
+  })
 
-  const data = dataRef;
-  const expandedKeys = expandedKeysRef;
-
+  /**
+   * 打开选择目录对话框
+   * */
   async function openDir(): string | null {
     const path = await open({
       multiple: false,
       directory: true,
     });
     if (path) {
-      const result = get_dir_children(path, 0)
+      let root = {
+        label: path.split('\\').pop(),
+        key: treeData.value.length + '0',
+        isLeaf: false,
+        children: []
+      }
+      // 获取用户选择的目录及所有子目录和子文件
+      root.children = await getDirChildren(path, root, treeData.value.length);
+      // 保存到Store中
+      await setStoreData('all-play-list', [...treeData.value, root], store.value);
+      // 筛选数据，只获取目录
+      root.children = getDirs(root.children);
+      // 更新到目录树中
+      treeData.value.push(root);
     }
   }
-  async function get_dir_children(path: string, index: number): Array {
+  /**
+   * 递归获取选择目录的所有子目录及文件
+   * @param path 目录路径
+   * @param root 根节点
+   * @param index 层级
+   * @returns Promise<Array> 子文件
+   * */
+  async function getDirChildren(path: string, root: any, index: number): Promise<Array> {
     let dir = [];
     if (path) {
+      index++;
       const entries = await readDir(path);
-      dir.push({
-        label: 'a',
-        key: '10',
-        children: []
-      })
       for (let i = 0; i < entries.length; i++) {
         let item = entries[i];
+        let child = {
+          label: item.name,
+          key: root.key + index.toString() + i,
+          isLeaf: item.isFile,
+          children: null
+        }
         if (item.isDirectory) {
-          dir.push({
-            label: item.name,
-            key: item.name,
-            children: []
-          })
-          get_dir_children(path + "/" + item.name)
+          child.children = await getDirChildren(path + "/" + item.name, child, index);
+        }
+        dir.push(child);
+      }
+    }
+    return dir;
+  }
+  /**
+   * 递归获取所有目录
+   * @param data 数据
+   * @returns Array<TreeOption> 所有目录
+   * */
+  function getDirs(data: Array<TreeOption>): Array<TreeOption> {
+    let dir = [];
+    if (data && Array.isArray(data)) {
+      for (let item of data) {
+        if (!item.isLeaf) {
+          item.children = getDirs(item.children);
+          dir.push(item);
+        }
+      }
+    }
+    return dir;
+  }
+  /**
+   * 递归获取所有目录
+   * @param data 数据
+   * @returns Array<TreeOption> 所有目录
+   * */
+  function getFiles(data: Array<TreeOption>): Array<TreeOption> {
+    let dir = [];
+    if (data && Array.isArray(data)) {
+      for (let item of data) {
+        if (!item.isLeaf) {
+          item.children = getFiles(item.children);
+          dir.push(item);
         }
       }
     }
     return dir;
   }
 
-  function renderSwitcherIcon() {
-      return h(NIcon, {"size": 18 }, { default: () => h(QueueMusicRound) })
-  }
-  function handleExpandedKeysChange(expandedKeys: string[]) {
-    expandedKeysRef.value = expandedKeys
-  }
+  /**
+   * 选中目录事件
+   * @param value 选中的目录名称
+   * */
   function handleSelect(value: string) {
     console.log('select', value)
     showDropdown.value = false
   }
+  /**
+   * 点击空白处隐藏操作目录弹出框
+   * */
   function handleClickOutside() {
     showDropdown.value = false
   }
+  function handleExpandedKeysChange(expandedKeys: string[]) {
+    expandedKeysRef.value = expandedKeys;
+  }
+  /**
+   * 播放列表目录点击右键弹出操作框
+   * @param node 点击的节点
+   * */
   function nodeProps({ option }: { option: TreeOption }) {
     return {
       onContextmenu(e: MouseEvent): void {
+        console.log('contextmenu', option)
         showDropdown.value = true;
         x.value = e.clientX
         y.value = e.clientY
@@ -129,7 +206,7 @@
   function handleDrop({ node, dragNode, dropPosition }: TreeDropInfo) {
     const [dragNodeSiblings, dragNodeIndex] = findSiblingsAndIndex(
         dragNode,
-        dataRef.value
+        treeData.value
     )
     if (dragNodeSiblings === null || dragNodeIndex === null)
       return
@@ -145,7 +222,7 @@
     else if (dropPosition === 'before') {
       const [nodeSiblings, nodeIndex] = findSiblingsAndIndex(
           node,
-          dataRef.value
+          treeData.value
       )
       if (nodeSiblings === null || nodeIndex === null)
         return
@@ -154,13 +231,13 @@
     else if (dropPosition === 'after') {
       const [nodeSiblings, nodeIndex] = findSiblingsAndIndex(
           node,
-          dataRef.value
+          treeData.value
       )
       if (nodeSiblings === null || nodeIndex === null)
         return
       nodeSiblings.splice(nodeIndex + 1, 0, dragNode)
     }
-    dataRef.value = Array.from(dataRef.value)
+    treeData.value = Array.from(treeData.value)
   }
 
   function findSiblingsAndIndex(
@@ -179,12 +256,39 @@
     }
     return [null, null]
   }
+  /**
+   * 目录前面的图标
+   * */
+  function renderSwitcherIcon(value) {
+    return h(NIcon, {"size": 18 }, { default: () => h(QueueMusicRound) })
+  }
+  /**
+   * 新增、修改、删除前面的图标
+   * */
   function renderIcon(icon: Component) {
     return () => {
       return h(NIcon, null, {
         default: () => h(icon)
       })
     }
+  }
+  /**
+   * 存储键值对到Store中
+   * @param key 键
+   * @param value 值
+   * @param store
+   * */
+  async function setStoreData(key: string, value: any, store: Store) {
+    await store.set(key, value);
+    await store.save();
+  }
+  /**
+   * 从Store中获取值
+   * @param key 键
+   * @param store
+   * */
+  async function getStoreData(key: string, store: Store) {
+    return await store.get(key);
   }
 </script>
 
